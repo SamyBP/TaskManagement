67f2ba8e1797997d10a514aaa1128da8555af742
SamyBP <b.pintea04@gmail.com>
2024-03-21 17:13:01 +0200

    Added user profile

--- TaskManagement/src/main/java/ro/app/taskmanagement/mappers/Mapper.java
+++ TaskManagement/src/main/java/ro/app/taskmanagement/mappers/Mapper.java
@@ -1,15 +1,11 @@
 package ro.app.taskmanagement.mappers;
 
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import org.springframework.stereotype.Component;
-import ro.app.taskmanagement.dtos.SignUpDto;
-import ro.app.taskmanagement.models.User;
 
 import java.lang.reflect.Field;
 
+
+@Component
 public interface Mapper<Destination> {
     default <Source> Destination createMap(Source source, Class<Destination> clazz) {
         Destination destination = null;
@@ -23,22 +19,6 @@ public interface Mapper<Destination> {
         Field[] sourceFields = source.getClass().getDeclaredFields();
 
         for (Field sourceField : sourceFields) {
-//            boolean flag = destinationField.isAnnotationPresent(Id.class)
-//                    | destinationField.isAnnotationPresent(OneToMany.class)
-//                    | destinationField.isAnnotationPresent(ManyToOne.class)
-//                    | destinationField.isAnnotationPresent(ManyToMany.class);
-//
-//            if (!flag) {
-//               try {
-//                   destinationField.setAccessible(true);
-//                   Field sourceField = source.getClass().getDeclaredField(destinationField.getName());
-//                   sourceField.setAccessible(true);
-//                   destinationField.set(destination, sourceField.get(source));
-//               } catch (NoSuchFieldException | IllegalAccessException e) {
-//                   throw new RuntimeException(e);
-//               }
-//            }
-
             try {
                 sourceField.setAccessible(true);
                 Field destiantionField = clazz.getDeclaredField(sourceField.getName());
--- TaskManagement/src/main/java/ro/app/taskmanagement/mappers/UserProfile.java
+++ TaskManagement/src/main/java/ro/app/taskmanagement/mappers/UserProfile.java
@@ -1,2 +1,8 @@
-package ro.app.taskmanagement.mappers;public class UserProfile {
+package ro.app.taskmanagement.mappers;
+
+import org.springframework.stereotype.Component;
+import ro.app.taskmanagement.models.User;
+
+@Component
+public class UserProfile implements Mapper<User> {
 }
